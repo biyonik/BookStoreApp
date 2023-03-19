6a3c918a534400bacc088826815c05ad2db003c6
ahmet <ahmet.altun60@gmail.com>
2023-03-19 16:01:42 +0300

    Request listener completed

---
+++ server/app/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const {StatusCodes} = require("http-status-codes");
+const cors = require('cors');
+const app = express();
+
+// Use middleware to form our contract for incoming json payloads ONLY!
+app.use(express.json());
+
+// Use middleware for url encoding
+app.use(express.urlencoded({extended: true}));
+
+// Use middleware to handle cors policy
+app.use(cors());
+
+// Actuator point
+app.get('/', (requestObject, responseObject, nextFunction) => {
+    responseObject.status(StatusCodes.OK)
+        .json({
+            'message': 'Service is up!'
+        });
+});
+
+// Routers
+// app.use('/register', registrationRouter)
+
+// Bad url or error can handle with middleware
+app.use((requestObject, responseObject, nextFunction) => {
+    const errorObject = new Error('Not found!');
+    errorObject.status = StatusCodes.NOT_FOUND;
+    nextFunction(errorObject);
+});
+
+app.use((errorObject, requestObject, responseObject, nextFunction) => {
+    responseObject.status(errorObject.status || StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({
+            error: {
+                message: errorObject.message,
+                status: errorObject.status
+            }
+        });
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const app = require("./app/app");
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
-http.createServer().listen(PORT, () => {
+http.createServer(app).listen(PORT, () => {
     console.log(`listening on port: ${PORT}`);
 });
